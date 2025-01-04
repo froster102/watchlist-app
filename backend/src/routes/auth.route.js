89d0db8dd324502588c1906b0aebd5dbdc82fa80
froster441 <froster441@gmail.com>
2025-01-04 09:50:11 +0530

    commit

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -5,6 +5,6 @@ const router = express.Router()
 
 router.post('/signin', authController.signin)
 router.post('/signup', authController.signup)
-router.get('/verify-email',authController.verifyEmail)
+router.get('/verify-email', authController.verifyEmail)
 
 export default router
